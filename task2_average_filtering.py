d00a35f12340b429f6bafcbf456b183a04d17af6
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-06-20 21:02:33 +0530

    task 2 done

---
+++ task2_average_filtering.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+def apply_mean_filter(image, ksize):
+    return cv2.blur(image, (ksize, ksize))
+
+if __name__ == "__main__":
+    image = cv2.imread("input.jpg")
+
+    for k in [3, 10, 20]:
+        blurred = apply_mean_filter(image, k)
+        cv2.imwrite(f"results/output_blur_{k}x{k}.jpg", blurred)
+        print(f"Saved results/output_blur_{k}x{k}.jpg")
